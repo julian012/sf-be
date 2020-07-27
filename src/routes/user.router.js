1ebadaa60a5b45cda88f5dd438d361c74736c6fa
Manuel Celis <manuelcelisforero@gmail.com>
2020-07-26 20:05:33 -0500

    Auto stash before revert of "Prueba"
    :c

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import User from "../../models/user";
-import { comparePassword, encryptPassword, generateToken, verifyToken } from '../utils/utils'
+import { comparePassword, encryptPassword, decryptPassword, generateToken, verifyToken } from '../utils/utils'
 
 
 const router = Router();
@@ -42,18 +42,19 @@ router.post('/regUser', async (req, res) => {
 
 router.post('/changePass', async (req, res) => {
     try {
-        const { user_mail, current_pass, new_pass } = await req.body;
-        const user = await User.update(
-            {
-                userPassword: new_pass
-            },{
-                where: {
-                    userMail: user_mail,
-                    userPassword: current_pass
-                }
-            })
-        if (user[0] === 0) throw new Error();
-        res.sendStatus(200)
+        const { user_mail, current_password, new_password } = await req.body;
+        const user = await User.findOne({where:{userMail: user_mail}})
+
+        if(user != null){
+            const pass = decryptPassword(user.userPassword)
+            user.update(
+                { userPassword: await encryptPassword(new_password) },
+                { where: {
+                    pass: current_password
+                }}
+            )
+            res.status(200).json(user_mail)
+        }
 
     } catch (e) {
         console.log(e.message)
@@ -66,8 +67,12 @@ router.post('/login', async (req, res) => {
         const {user_mail, user_password} = await req.body;
         const user = await User.findOne({where: {userMail: user_mail }});
 
-        if (!user && !await comparePassword(user_password, user.userPassword)) throw new Error();
-        res.status(200).json({ token: await generateToken(user.id, user.userMail)})
+        if (user && await comparePassword(user_password, user.userPassword)){
+            res.status(200).json({ token: await generateToken(user.id, user.userMail)}) 
+        }else{
+            throw new Error();
+        }
+        
     } catch (e) {
         res.status(422).send({errors: {email: 'Datos Incorrectos'}})
     }
@@ -86,10 +91,23 @@ router.post('/recoverPassword', async (req, res) => {
         if(mail === null){
             res.status(422).send({errors: {email: 'El correo no esta registrado'}})
         }else{
-            res.status(200).json(mail)
+            res.status(200).json(user_mail)
         }
     }catch(e){
         res.status(422).send({errors: {email: 'Datos Incorrectos'}})
+    }
+})
+
+router.post('/deleteUser', async(req, res) => {
+    try{
+        const {user_mail} = await req.body;
+        await User.destroy({
+            where: {
+                userMail: user_mail
+            }
+        })
+    }catch(e){
+        res.status(422).send({errors: {email: 'Usuario no encontrado'}})
     }
 })
 
