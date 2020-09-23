4d6d30d31d026f1bf04dcf644c453ec034a8869e
Manuel <manuelcelisforero@gmail.com>
2020-09-23 14:17:28 -0500

    Se creo el servicio getTimeWorkerByWorkersByOuvreId

--- src/routes/assignworker.router.js
+++ src/routes/assignworker.router.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import AssignWorker from "../../models/assignworker";
 import Schedule from '../../models/schedule'
+import Task from '../../models/task'
 import {verifyToken, Op} from '../utils/utils';
 import User from "../../models/user";
 
@@ -20,9 +21,94 @@ router.get('/', verifyToken, async (req, res) => {
 
 router.get('/getTimeWorkerByWorkersByOuvreId', verifyToken, async (req, res) => {
     try{
+        const ouvreId = req.query.ouvreId
+        const startDate = new Date(req.query.initDate)
+        const endDate = new Date(req.query.endDate)
+        var response = []
+        const tasks = await Task.findAll({
+            raw: true,
+            where: {
+                ouvreId: ouvreId
+            }
+        })
+        var userIds = []
+        const allAssigns = []
+        for (let t = 0; t < tasks.length; t++) {
+            const element = tasks[t];
+            const assigns = await AssignWorker.findAll({
+                raw: true,
+                where:{
+                    taskId: element.id
+                }
+            })           
+            for (let a = 0; a < assigns.length; a++) {
+                const as = assigns[a];
+                allAssigns.push(as)
+            }
+        }
+        for (let a = 0; a < allAssigns.length; a++) {
+            const element = allAssigns[a];
+            userIds.push(element.userId)
+        }
         
-    }catch(e){
+        for (let u = 0; u < userIds.length; u++) {
+            const id = userIds[u];
+            var user = await User.findOne({
+                raw: true,
+                where:{
+                    id: id
+                }
+            })
+            var schedules = await Schedule.findAll({
+                raw: true,
+                where: {
+                    userId: id,
+                    scheduleDate: {
+                        [Op.between]: [startDate, endDate]
+                    }
+                }
+            })
+            schedules.sort(function(a,b){
+                return a.scheduleDate.getTime() - b.scheduleDate.getTime()
+            });
+            var totalWorkerHours = 0
+            var totalWorkDays = []
+            var dayInWeek = []
+            for (let j = 0; j < schedules.length; j++) {
+                const schedule = schedules[j];
+                dayInWeek.push(schedule)
+                if (dayInWeek.length > 3) {
+                    var day = {}
+                    day.date = dayInWeek[0].scheduleDate.toISOString().split("T")[0]
+                    var morningDate = dayInWeek[0].scheduleDate
+                    var halfDayDate = dayInWeek[1].scheduleDate
+                    var afternoonDate = dayInWeek[2].scheduleDate
+                    var nightDate = dayInWeek[3].scheduleDate
 
+                    var difInTimeMorning = halfDayDate.getTime() - morningDate.getTime()
+                    var difInTimeAfternoon = nightDate.getTime() - afternoonDate.getTime()
+                    var dayInHours = (difInTimeMorning / (1000 * 3600)) + (difInTimeAfternoon / (1000 * 3600))
+                    day.workedHours = dayInHours
+                    totalWorkerHours += dayInHours
+
+                    totalWorkDays.push(day)
+                    
+                    dayInWeek.length = 0
+                }
+            }
+            delete user['userPassword']
+            delete user['updatedAt']
+            user.totalWorkerHours = totalWorkerHours
+            user.work = totalWorkDays
+            response.push(user)
+        }
+        res.status(200).json(response)
+
+    }catch(e){
+        console.log(e)
+        res.status(422).json({
+            message: 'error'
+        })
     }
 })
 
@@ -83,11 +169,8 @@ router.get('/getTimeWorkedByWorkerId', verifyToken, async (req, res) => {
         delete user['updatedAt']
         user.totalWorkerHours = totalWorkerHours
         user.work = totalWorkDays
-        console.log(user)
 
-        res.status(422).json({
-            message: 'error'
-        })
+        res.status(200).json(user)
     }catch(e){
         console.log(e)
         res.status(422).json({
@@ -200,81 +283,5 @@ router.post('/transferWorker', verifyToken, async (req, res) => {
         res.status(422).json({message: 'No se pudo completar la accion'})
     }
 })
-
-router.get('/getTimeWorkedByWorker', verifyToken, async (req, res) => {
-    try{
-       const assigns = await AssignWorker.findAll({});
-       var ids = [];
-       for(var i = 0; i < assigns.length; i++){
-            if(!(verifyId(ids, assigns[i].userId))){
-                ids.push(assigns[i].userId);
-            }
-       }
-       var result = [];
-       for (var j = 0; j < ids.length; j++){
-            var total = 0;
-            for(var i = 0; i < assigns.length; i++){
-                if(ids[j] === assigns[i].userId){
-                    if(assigns[i].assignEndDate === null){
-                        const date = new Date();
-                        const dateFirst = new Date(assigns[i].assignStartDate);
-                        var daysNoCount = getBusinessDays(dateFirst, date); 
-                        var daysDiff = date.getTime() - dateFirst.getTime();
-                        var daysTotal = Math.round(daysDiff/(1000*60*60*24));
-                        total += (daysTotal - daysNoCount) * 8;
-                    }else{
-                        const dateEnd = new Date(assigns[i].assignEndDate);
-                        const dateFirst = new Date(assigns[i].assignStartDate);
-                        var daysNoCount = getBusinessDays(dateFirst, dateEnd)
-                        var daysDiff = dateEnd.getTime() - dateFirst.getTime();
-	                    var daysTotal = Math.round(daysDiff/(1000*60*60*24));
-                        total += (daysTotal - daysNoCount) * 8;
-                    }
-                }
-            }
-            result.push({id: ids[j], total: total})
-       }
-       var  finalNames = [];
-       var series = [];
-       const workers = await User.findAll({});
-       for(var j = 0; j < workers.length; j++){
-            for(var i = 0; i < result.length; i ++){
-                if(result[i].id === workers[j].id){
-                    finalNames.push(workers[j].userName)
-                }
-            }
-       }
-       for(var i = 0; i < result.length; i++){
-           series.push(result[i].total);
-       }
-       res.status(200).json({labels: finalNames, series: series});
-    }catch(e){
-        console.log(e);
-        res.status(422).json({error: e});
-    }
-})
-
-function getBusinessDays(dateInit, dateFinal){
-    var days = 0;
-    var dateI = new Date(dateInit);
-    var dateF = new Date(dateFinal); 
-    while(dateI <= dateF){
-        if(dateI.getDay() == 0 || dateI.getDay() == 6){
-            days++;
-        }
-        dateI.setDate(dateI.getDate() + 1);
-    }
-    return days;
-}
-
-function verifyId(array, id){
-    for(var p = 0; p < array.length; p++){
-        if (array[p] === id){
-                
-            return true;
-        }
-    }
-    return false;
-}
 
 export default router;
